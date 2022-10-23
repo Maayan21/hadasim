6421b4ab85b39e4ffb6d44d764d3ca26fa06b00f
Maayan21 <76556838+Maayan21@users.noreply.github.com>
2022-10-23 18:39:39 +0300

    Create config.js

---
+++ src/config/config.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+
+module.exports = {
+  "development": {
+    "username": "postgres",
+    "password": "superuser",
+    "database": "postgres",
+    "host": "localhost",
+    "dialect": "postgres"
+  },
+  "test": {
+    "username": "postgres",
+    "password": "superuser",
+    "database": "postgres",
+    "host": "localhost",
+    "dialect": "postgres"
+  },
+  "production": {
+    "username": "postgres",
+    "password": "superuser",
+    "database": "postgres",
+    "host": "localhost",
+    "dialect": "postgres"
+  }
+}
+
+
