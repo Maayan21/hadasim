40e63e955a0447fa17bd23495e57f2d4b0de4322
Maayan21 <76556838+Maayan21@users.noreply.github.com>
2022-10-23 19:09:59 +0300

    Delete server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-const app = require('./app')
-const dotenv = require("dotenv").config();
-
-const PORT = process.env.PORT || 5000;
-
-const server = app.listen(PORT, () => {
-    console.log(`listhen ${PORT}`)
-})
